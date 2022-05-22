4895e8d9de81924282f347babd5231ba7be7ef70
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-22 14:25:16 +0200

    set all routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+->namespace('Admin')
+->prefix('admin')
+->name('admin.')
+->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
+
+Route::get('/{any}', 'Guest\HomeController@index')->where('any','.*');
