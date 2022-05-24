8ff6049058969d5d7f60255ecac10ab0b017c6ea
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-24 14:37:33 +0200

    update seeders

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -10,9 +10,10 @@ $factory->define(Post::class, function (Faker $faker) {
 
     $title = $faker->sentence;
     $slug = Str::slug($title, '-');
+    $user_ids = User::pluck('id')->toArray();
 
     return [
-        'user_id'=> $faker->unique()->numberBetween(1, User::count()),
+        'user_id'=> $faker->randomElement($user_ids),
         'title' => $title,
         'slug' => $slug,
         'description' => $faker->paragraph,
--- database/factories/UserInfoFactory.php
+++ database/factories/UserInfoFactory.php
@@ -7,8 +7,11 @@ use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(UserInfo::class, function (Faker $faker) {
+
+    $user_ids = User::pluck('id')->toArray();
+
     return [
-        'user_id'=> $faker->unique()->numberBetween(1, User::count()),
+        'user_id'=> $faker->unique()->randomElement($user_ids),
         'date_of_birth' => $faker->date(),
         'address' => $faker->address(),
     ];
